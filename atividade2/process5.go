a8ee75ae317dd9e789d3054d6c4f3c135cb93bb1
dennysrocha <dennysrocha.1994@gmail.com>
2018-08-30 22:07:49 -0300

    Finalizacao da Tarefa 5

--- atividade2/process5.go
+++ atividade2/process5.go
@@ -13,16 +13,16 @@ import (
 //Variáveis globais interessantes para o processo
 var err string
 var myPort string //porta do meu servidor
-var myProcess int //
+var myProcess int //numero do meu processo
 var nServers int //qtde de outros processo
 var CliConn []*net.UDPConn //vetor com conexões para os servidores dos outros processos
 var ServConn *net.UDPConn //conexão do meu servidor (onde recebo mensagens dos outros processos)
-//var logicalClock int
-var data DataClocks
+var Data DataClocks
 
-type DataClocks struct {
-	id int
-	logicalClock []int
+//Estruturas para o processo
+type DataClocks struct { //crio a estrutura para o vector timestamp (aqui chamado de VM)
+	Id int
+	LogicalClock []int
 }
 
 func CheckError(err error) {
@@ -41,21 +41,47 @@ func PrintError(err error) {
 func doServerJob() {
 	//Ler (uma vez somente) da conexão UDP a mensagem
 	buf := make([]byte, 1024)
-	n,addr,err := ServConn.ReadFromUDP(buf)
+	n,_,err := ServConn.ReadFromUDP(buf)
+	CheckError(err)
 
-	var dataReceived DataClocks
-	err = json.Unmarshal(buf[:n], &dataReceived)
-	PrintError(err)
-
-	//Escreve na tela a msg recebida
-	if data.logicalClock[myProcess]>dataReceived.logicalClock[dataReceived.id] {
-		logicalClock = 1+clock
-	} else {
-		logicalClock++
+	//Criar uma struct para futuramente armazenar os dados recebidos
+	var DataReceived DataClocks
+	DataReceived.Id = 0
+	for i:=0; i<nServers+1; i++ { //aloco espaco para cada dimensao do meu vetor
+		DataReceived.LogicalClock = append(DataReceived.LogicalClock, 1)
 	}
-	fmt.Println("Recebi um logicalClock igual a ",clock, " do ",addr)
-	fmt.Println("Agora meu logicalClock eh ",logicalClock)
+
+	err = json.Unmarshal(buf[:n], &DataReceived) //interpreto por meio do json e passo pra estrutura de dados
 	PrintError(err)
+
+	fmt.Println("Recebi msg. O VT recebido eh: ", DataReceived.LogicalClock)
+	fmt.Println("Meu VT era: ", Data.LogicalClock)
+	//Escreve na tela a msg recebida
+	for i:=0; i<nServers+1; i++ { //comparo e sempre pego o maior
+		if DataReceived.LogicalClock[i] > Data.LogicalClock[i] {
+			Data.LogicalClock[i] = DataReceived.LogicalClock[i]
+		}
+	}
+	Data.LogicalClock[myProcess-1]++
+	fmt.Println("Agora, meu VT eh ", Data.LogicalClock)
+	fmt.Println("-----------------------------")
+	PrintError(err)
+}
+
+func doClientJob(x int) {
+	Data.LogicalClock[myProcess-1]++
+	fmt.Println("Meu VT eh: ", Data.LogicalClock)
+	fmt.Printf("Enviando msg pro proc %v\n", x)
+	fmt.Println("-----------------------------")
+	if x!=myProcess {
+		jsonRequest, err := json.Marshal(Data) //reescrevo os dados por meio do json
+		CheckError(err)
+		if x>myProcess {
+			x--
+		}
+		_, err = CliConn[x-1].Write(jsonRequest) //envio os dados reescritos pelo canal
+		PrintError(err)
+	}
 }
 
 func readInput(ch chan int) {
@@ -68,17 +94,17 @@ func readInput(ch chan int) {
 	}
 }
 
+
+/////////////////////////////////////////////ABAIXO//////////////////////////////////////////////////////////////
+
 func initConnections() {
 	myProcess, _ = strconv.Atoi(os.Args[1])
-	fmt.Println("A variavel myProcess recebeu", myProcess)
+	fmt.Println("Este eh o processo ", myProcess)
 	myPort = os.Args[myProcess+1]
 	nServers = len(os.Args) - 3
-	data := DataClocks {
-		id: myProcess,
-		logicalClock: make([]int, nServers+1),
-	}
 	/*Esse 3 tira o nome (no caso Process), o numero do (meu) processo e tira a porta que é minha. As demais portas são dos outros processos*/
 
+	Data.Id = myProcess
 
 	//Outros códigos para deixar ok a conexão do meu servidor
 	ServAddr,err := net.ResolveUDPAddr("udp","127.0.0.1"+myPort)
@@ -88,12 +114,12 @@ func initConnections() {
 	CliConn = make([]*net.UDPConn, nServers)
 
 	//Outros códigos para deixar ok as conexões com os servidores dos outros processos
-	j:=0
+	j:=0 //esse j eh apenas para "pular" o i correspondente ao meu servidor
 	for i:=0; i<nServers+1; i++ {
-		data.logicalClock[i]=1
-		fmt.Println("inicializei o LC do id ", i ," com o valor de ", data.logicalClock[i])
+		Data.LogicalClock = append(Data.LogicalClock, 1)
 		if i!=myProcess-1 {
 			ServAddr,err = net.ResolveUDPAddr("udp","127.0.0.1"+os.Args[i+2])
+			CheckError(err)
 			LocalAddr, err := net.ResolveUDPAddr("udp","127.0.0.1:0")
 			CheckError(err)
 			CliConn[j], err = net.DialUDP("udp", LocalAddr, ServAddr)
@@ -101,10 +127,13 @@ func initConnections() {
 			j++
 		}
 	}
+	fmt.Println("Conexoes inicializadas")
+	fmt.Println("-----------------------------")
 }
 
+/////////////////////////////////////////////ACIMA///////////////////////////////////////////////////////////////
+
 func main() {
-	logicalClock=1
 	initConnections()
 	//O fechamento de conexões devem ficar aqui, assim só fecha conexão quando a main morrer
 	defer ServConn.Close()
@@ -121,19 +150,7 @@ func main() {
 		select {
 			case x, valid := <-ch:
 				if valid {
-					fmt.Printf("Enviei para o processo %v ", x)
-					fmt.Printf("o meu struct data\n")
-					buf := []byte(strconv.Itoa(logicalClock))
-					if x!=myProcess {
-						if x>myProcess {
-							x--
-						}
-						_, err := CliConn[x-1].Write(buf)
-						PrintError(err)
-					} else {
-						logicalClock++
-						fmt.Println("Estou incrementando meu logicalClock em 1 unidade. Agora ele vale logicalClock =",logicalClock)
-					}
+					go doClientJob(x)
 				} else {
 					fmt.Println("Channel closed!")
 				}
