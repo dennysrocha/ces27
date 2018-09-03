17ea1d9556533aa3b4a2d2c5b1cfabed597a6313
dennysrocha <dennysrocha.1994@gmail.com>
2018-09-03 15:23:13 -0300

    Programa pronto. Talvez eu ainda edite o campo "Recebi: S(n)" para indicar de onde a msg veio

--- atividade3/changroberts.go
+++ atividade3/changroberts.go
@@ -25,6 +25,7 @@ var ServConn *net.UDPConn //conexão do meu servidor (onde recebo mensagens dos 
 type Message struct { //crio a estrutura para o vector timestamp (aqui chamado de VM)
 	Id int
 	Type string
+	NumMessage int
 }
 
 var MessageReceived Message
@@ -42,6 +43,16 @@ func PrintError(err error) {
 	}
 }
 
+func finalizarProcessos() {
+	jsonRequest, err := json.Marshal(MessageReceived) //reescrevo os dados por meio do json
+	CheckError(err)
+	for i:=0; i<nServers; i++ { //aqui cada servidor na conexao recebe a mensagem
+		time.Sleep(time.Second)
+		_,err := CliConn[i].Write(jsonRequest)
+		PrintError(err)
+	}
+}
+
 func doServerJob() {
 	//Ler (uma vez somente) da conexão UDP a mensagem
 	buf := make([]byte, 1024)
@@ -51,37 +62,58 @@ func doServerJob() {
 	err = json.Unmarshal(buf[:n], &MessageReceived) //interpreto por meio do json e passo pra estrutura de dados
 	PrintError(err)
 
+	if MessageReceived.NumMessage==-1 {
+		fmt.Print("O lider eh o processo de ID ", leaderID, "\n")
+		fmt.Println("Finalizando o programa...")
+		os.Exit(0)
+	}
+
+	fmt.Print("Mensagem de numero ", MessageReceived.NumMessage, "\n")
 	fmt.Print("Recebi: ", MessageReceived.Type, MessageReceived.Id, "\n") //Escreve na tela a msg recebida
 	
 	if MessageReceived.Type=="S" {
-		go stage1(MessageReceived.Id)
+		go stage1()
 	} else if MessageReceived.Type=="F" {
-		go stage2(MessageReceived.Id)
+		isParticipant = false
+		leaderID = MessageReceived.Id
+		if leaderID == myID {
+			fmt.Print("Eu sou o lider: ID ", myID, ".\nFinalizando o programa...")
+			MessageReceived.NumMessage = -1
+			finalizarProcessos()
+			os.Exit(0)
+			return
+		}
+		go stage2()
 	} else {
-		fmt.Println("Entrada invalida. Fim do programaa")
+		fmt.Println("Entrada invalida. Fim do programa")
 		os.Exit(0)
 	}
-	fmt.Println("-----------------------------")
 	PrintError(err)
 }
 
-func stage1(receivedID int) {
+func stage1() {
 	time.Sleep(time.Second)
-	if receivedID<myID {
-		receivedID = myID
-	} else if receivedID==myID && isParticipant{
+	if MessageReceived.Id<myID { // meu ID eh maior, entao passe o meu ID
+		MessageReceived.Id = myID
+		if !isParticipant { // soh passe o ID se eu ainda nao tiver mandado o meu
+			isParticipant = true
+			doClientJob((myProcess+1)%(nServers+1))
+		}
+	} else if MessageReceived.Id==myID && isParticipant{ // mesmo ID que o meu
 		leaderID = myID
-		stage2(receivedID)
+		go stage2()
+		return
+	} else { // [meu ID eh menor] ou [eh igual e eu nao sou participante], entao passe o ID recebido
+		isParticipant = true
+		doClientJob((myProcess+1)%(nServers+1))
 	}
-	isParticipant = true
-	doClientJob((myID+1)%(nServers+1))
 }
 
-func stage2(receivedID int) {
+func stage2() {
 	time.Sleep(time.Second)
 	isParticipant = false // nem precisava, a booleana ja vem como false
 	MessageReceived.Type = "F"
-	doClientJob((myID+1)%(nServers+1))
+	doClientJob((myProcess+1)%(nServers+1))
 }
 
 func doClientJob(x int) {
@@ -89,12 +121,14 @@ func doClientJob(x int) {
 		x = nServers + 1
 	}
 	fmt.Print("Enviando ", MessageReceived.Type, MessageReceived.Id)
+	MessageReceived.NumMessage++ // aumenta o numero da msg
 	fmt.Println("\n-----------------------------")
 	jsonRequest, err := json.Marshal(MessageReceived) //reescrevo os dados por meio do json
 	CheckError(err)
 	if x>myProcess {
 		x--
 	}
+	time.Sleep(time.Second)
 	_, err = CliConn[x-1].Write(jsonRequest) //envio os dados reescritos pelo canal
 	PrintError(err)
 }
@@ -122,7 +156,7 @@ func initConnections() {
 	reader := bufio.NewReader(os.Stdin)
 	text, _, _ := reader.ReadLine()
 	myID, _ = strconv.Atoi(string(text))
-
+	MessageReceived.NumMessage = 0
 
 	//Outros códigos para deixar ok a conexão do meu servidor
 	ServAddr,err := net.ResolveUDPAddr("udp","127.0.0.1"+myPort)
@@ -170,7 +204,7 @@ func main() {
 					fmt.Println("Enviando start")
 					MessageReceived.Type = "S"
 					MessageReceived.Id = myID
-					go stage1(myID)
+					go stage1()
 				} else {
 					fmt.Println("Channel closed!")
 				}
